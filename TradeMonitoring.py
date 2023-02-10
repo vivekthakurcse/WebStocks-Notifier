16bb335e3347a482e1ae564e322912d5522e95e3
Vivek Thakur <vivekthakurcse779@gmail.com>
2023-02-10 20:06:03 +0530

    Add files via upload

--- TradeMonitoring.py
+++ TradeMonitoring.py
@@ -1,6 +1,7 @@
 import asyncio
 import json
 import websockets
+import smtplib
 
 async def subscribe_to_stock():
     async with websockets.connect('ws://enter-your-stock-url') as websocket:
@@ -8,14 +9,30 @@ async def subscribe_to_stock():
         await websocket.send(subscribe_message)
         current_price = None
         async for message in websocket:
-            trade_data = json.loads(message)
-            if current_price and trade_data["price"] < current_price:
-                current_price = trade_data["price"]
-                send_notification("Stock price is falling")
-            else:
-                current_price = trade_data["price"]
-                
-def send_notification(message):
-    # this section is pending 
+            try:
+                trade_data = json.loads(message)
+                if current_price and trade_data["price"] < current_price:
+                    current_price = trade_data["price"]
+                    send_notification("Stock price is falling")
+                else:
+                    current_price = trade_data["price"]
+            except Exception as e:
+                print(f"Error processing message: {e}")
 
-asyncio.get_event_loop().run_until_complete(subscribe_to_stock())
+def send_notification(message):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login("your-email-address@gmail.com", "your-email-password")
+        subject = "Stock Price Alert"
+        body = message
+        msg = f"Subject: {subject}\n\n{body}"
+        server.sendmail("your-email-address@gmail.com", "to-email-address@gmail.com", msg)
+        print(f"Successfully sent email to {to_email}")
+        server.quit()
+    except Exception as e:
+        print(f"Error sending email: {e}")
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(subscribe_to_stock())
